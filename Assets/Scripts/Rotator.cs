0bb13c040ae660d88b3b6db70110edd6a169607a
ayobami <mrayobamioluwaseyi@gmail.com>
2022-10-12 04:30:21 +0100

    Update Rotator.cs
    This is the solution for Challenge 7

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -13,18 +13,18 @@ public class Rotator : MonoBehaviour
         if(!GameManager.isGameStarted)
         return;
         // For pc 
-       if (Input.GetMouseButton(0))
+     /*  if (Input.GetMouseButton(0))
         {
             float mouseX = Input.GetAxisRaw("Mouse X");
             transform.Rotate(0, -mouseX  * rotationSpeed * Time.deltaTime, 0);
         }
-        /*
+        */
         // For Mobile
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             float xDelta = Input.GetTouch(0).deltaPosition.x;
                         transform.Rotate(0, -xDelta  * rotationSpeed * Time.deltaTime, 0);
 
-        }*/
+        }
     }
 }
